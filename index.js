02203db1c622f865c343008988df72c610ea62b5
Bridget <bridget.njoki@student.moringaschool.com>
2024-06-29 23:57:30 +0300

    add

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance) {
   // Write your code here!
-}
+  if(distance=> 400) {
+return 'This one is on me!';
+} else if (distance > 400 & distance<= 2000){
+  return "That will be twenty bucks";
+} else {
+  return "No can do.";
 
 function ternaryCheckCity(){
   // Write your code here!
